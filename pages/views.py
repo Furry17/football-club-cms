b5e1391d4acf810b73e15bfc0c6a7ac27919ba69
Martin Furár <furry.martin@gmail.com>
2026-08-10 13:21:01 +0200

    Create homepage layout with Bootstrap

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def homepage(request):
-    return HttpResponse('Football Club CMS')
+    return render(request, 'pages/home.html')
